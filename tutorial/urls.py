81205dca2cda2a749ce7d28b00c2ac5adda3bed8
smrha <lord.smrha@gmail.com>
2020-08-23 20:48:17 +0430

    Edit git files

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls'))
+    path('account/', include('accounts.urls')),
+    path('', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
